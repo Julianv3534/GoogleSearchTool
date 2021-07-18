4c74e6e09b483e9336233de52b3fb0f4343db729
Julian <69217557+Julianv3534@users.noreply.github.com>
2021-07-18 21:15:25 +0200

    Update GoogleSearch.py

--- GoogleSearch.py
+++ GoogleSearch.py
@@ -4,6 +4,6 @@ from googlesearch import search
 
 request = input("What do you want to search in google?")
 
-#you can change the stop number to a larger or bigger list
+#you can change the stop number to a biggeror smaller list
 for i in search(request, start = 0, stop = 8):
     print (i)
